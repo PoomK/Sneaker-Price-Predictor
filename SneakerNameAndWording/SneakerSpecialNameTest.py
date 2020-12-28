fe74629ce08dea9d394d21df6c69231d6bf2bdb0
Poom Kijoran <triplepoom@hotmail.com>
2020-12-28 16:01:01 +0700

    Worked on special name testing

--- SneakerNameAndWording/SneakerSpecialNameTest.py
+++ SneakerNameAndWording/SneakerSpecialNameTest.py
@@ -1,5 +1,17 @@
 SpecialNameArr = ["Jordan 1 Retro High","adidas Yeezy Boost 350 V2","Jordan 1 Mid","Nike Air Force 1"]
-SneakerName = "adidas Yeezy Boost 350 V2 Black Red (2017/2020)"
+yeezyBredName = "adidas Yeezy Boost 350 V2 Black Red (2017/2020)"
 
-def splitSpecial():
-    
+def strip(s):
+    return s.strip()
+
+def splitSpecial(sneakerName):
+    for s in sneakerName:
+        if s in sneakerName:
+            nameArray = sneakerName.split(s)
+            nameArray.insert(1, s)
+            nameArray = list(map(strip, nameArray))
+            return nameArray
+        return sneakerName
+
+a = splitSpecial(yeezyBredName)
+print(a)
