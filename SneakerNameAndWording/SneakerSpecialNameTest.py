c54e85e5620ddf0ab2b3d8f78e3cd86ad9dbcf29
Poom Kijoran <triplepoom@hotmail.com>
2020-12-28 16:12:41 +0700

    Updated

--- SneakerNameAndWording/SneakerSpecialNameTest.py
+++ SneakerNameAndWording/SneakerSpecialNameTest.py
@@ -5,13 +5,36 @@ def strip(s):
     return s.strip()
 
 def splitSpecial(sneakerName):
-    for s in sneakerName:
+    #iterate through specialNames
+    for s in specialNames:
+        #check if special name is in sneaker name
         if s in sneakerName:
             nameArray = sneakerName.split(s)
             nameArray.insert(1, s)
             nameArray = list(map(strip, nameArray))
-            return nameArray
-        return sneakerName
 
-a = splitSpecial(yeezyBredName)
-print(a)
+            #Check for space in 1st element and split if there is space
+            if "" in nameArray[0]:
+                element1 = nameArray[0]
+                del nameArray[0]
+                newArr1 = element1.split()
+                for i in range(len(newArr1)):
+                    position1 = i
+                    value1 = newArr1[i]
+                    nameArray.insert(position1,value1)
+
+            #Check for space in 3rd element and split if there is space
+            if "" in nameArray[2]:
+                element2 = nameArray[2]
+                del nameArray[2]
+                newArr2 = element2.split()
+                for i in range(len(newArr2)):
+                    position2 = 2+i
+                    value2 = newArr2[i]
+                    nameArray.insert(position2,value2)
+
+            return nameArray
+    return sneakerName
+
+    a = splitSpecial(yeezyBredName)
+    print(a)
