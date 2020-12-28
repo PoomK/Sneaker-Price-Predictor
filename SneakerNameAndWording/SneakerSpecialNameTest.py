aff7adaa2fd715d0f45801e5ed06ef927e4c089a
Poom Kijoran <triplepoom@hotmail.com>
2020-12-28 15:24:51 +0700

    Started special name testing

---
+++ SneakerNameAndWording/SneakerSpecialNameTest.py
@@ -0,0 +1,5 @@
+SpecialNameArr = ["Jordan 1 Retro High","adidas Yeezy Boost 350 V2","Jordan 1 Mid","Nike Air Force 1"]
+SneakerName = "adidas Yeezy Boost 350 V2 Black Red (2017/2020)"
+
+def splitSpecial():
+    
