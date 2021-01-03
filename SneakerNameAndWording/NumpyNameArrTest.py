59dac1c854596170369edda09e62c621ba564e00
Poom Kijoran <triplepoom@hotmail.com>
2021-01-03 19:01:40 +0700

    Updated file so that numpy array can be created

--- SneakerNameAndWording/NumpyNameArrTest.py
+++ SneakerNameAndWording/NumpyNameArrTest.py
@@ -8,3 +8,21 @@ with open("NumpySneakerNames.txt", "r") as f:
         SneakerNamesArr.append(SneakerName)
 
 print(SneakerNamesArr)
+
+#Sneaker name arr for testing
+YzyBredArr = ["adidas", "Yeezy Boost 350 V2", "Black", "Red", "(2017/2020)"]
+Jordan1MochaArr = ["Air", "Jordan 1 Retro High", "Dark", "Mocha"]
+TrueCount = 0 #Counter
+NumpyArr = [] #Numpy array for sneaker
+
+for i in range(len(SneakerNamesArr)):
+    if Jordan1MochaArr[TrueCount] == SneakerNamesArr[i]:
+        NumpyArr.append(1)
+        if TrueCount == len(Jordan1MochaArr)-1:
+            TrueCount = TrueCount
+        else:
+            TrueCount = TrueCount + 1
+    else:
+        NumpyArr.append(0)
+
+print(NumpyArr)
