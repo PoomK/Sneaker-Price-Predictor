f61f4939197393ed554e411f647334ec19acd7d0
Poom Kijoran <triplepoom@hotmail.com>
2021-01-03 19:11:33 +0700

    Update getnumpyarr file

--- SneakerNameAndWording/NumpyNameArrTest.py
+++ SneakerNameAndWording/NumpyNameArrTest.py
@@ -1,28 +1,32 @@
-#Returns an array of sneaker names from numpysneakernames.txt in specific order
-SneakerNamesArr = []
-
-with open("NumpySneakerNames.txt", "r") as f:
-    lines = f.readlines()
-    for line in lines:
-        SneakerName = line.strip("\n")
-        SneakerNamesArr.append(SneakerName)
-
-print(SneakerNamesArr)
-
-#Sneaker name arr for testing
 YzyBredArr = ["adidas", "Yeezy Boost 350 V2", "Black", "Red", "(2017/2020)"]
 Jordan1MochaArr = ["Air", "Jordan 1 Retro High", "Dark", "Mocha"]
-TrueCount = 0 #Counter
-NumpyArr = [] #Numpy array for sneaker
 
-for i in range(len(SneakerNamesArr)):
-    if Jordan1MochaArr[TrueCount] == SneakerNamesArr[i]:
-        NumpyArr.append(1)
-        if TrueCount == len(Jordan1MochaArr)-1:
-            TrueCount = TrueCount
+def GetNumpyArray():
+
+    #Returns an array of sneaker names from numpysneakernames.txt in specific order
+    SneakerNamesArr = []
+
+    with open("NumpySneakerNames.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            SneakerName = line.strip("\n")
+            SneakerNamesArr.append(SneakerName)
+
+    #Sneaker name arr for testing
+    YzyBredArr = ["adidas", "Yeezy Boost 350 V2", "Black", "Red", "(2017/2020)"]
+    Jordan1MochaArr = ["Air", "Jordan 1 Retro High", "Dark", "Mocha"]
+    TrueCount = 0 #Counter
+    NumpyArr = [] #Numpy array for sneaker
+
+    #For loop to check if the value is true in sneaker name arr, returns 1 if true and 0 if false
+    for i in range(len(SneakerNamesArr)):
+        if Jordan1MochaArr[TrueCount] == SneakerNamesArr[i]:
+            NumpyArr.append(1)
+            if TrueCount != len(Jordan1MochaArr)-1:
+                TrueCount = TrueCount + 1
         else:
-            TrueCount = TrueCount + 1
-    else:
-        NumpyArr.append(0)
+            NumpyArr.append(0)
 
-print(NumpyArr)
+    return NumpyArr
+
+print(GetNumpyArray())
