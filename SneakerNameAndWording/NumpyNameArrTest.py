d47c94f05c71e1e2cb518d4c651208d56dac505d
Poom Kijoran <triplepoom@hotmail.com>
2021-01-03 19:13:34 +0700

    Created a function getnumpyarray which take in arr

--- SneakerNameAndWording/NumpyNameArrTest.py
+++ SneakerNameAndWording/NumpyNameArrTest.py
@@ -1,7 +1,7 @@
 YzyBredArr = ["adidas", "Yeezy Boost 350 V2", "Black", "Red", "(2017/2020)"]
 Jordan1MochaArr = ["Air", "Jordan 1 Retro High", "Dark", "Mocha"]
 
-def GetNumpyArray():
+def GetNumpyArray(SpecificNameArr):
 
     #Returns an array of sneaker names from numpysneakernames.txt in specific order
     SneakerNamesArr = []
@@ -13,20 +13,19 @@ def GetNumpyArray():
             SneakerNamesArr.append(SneakerName)
 
     #Sneaker name arr for testing
-    YzyBredArr = ["adidas", "Yeezy Boost 350 V2", "Black", "Red", "(2017/2020)"]
-    Jordan1MochaArr = ["Air", "Jordan 1 Retro High", "Dark", "Mocha"]
     TrueCount = 0 #Counter
     NumpyArr = [] #Numpy array for sneaker
 
     #For loop to check if the value is true in sneaker name arr, returns 1 if true and 0 if false
     for i in range(len(SneakerNamesArr)):
-        if Jordan1MochaArr[TrueCount] == SneakerNamesArr[i]:
+        if SpecificNameArr[TrueCount] == SneakerNamesArr[i]:
             NumpyArr.append(1)
-            if TrueCount != len(Jordan1MochaArr)-1:
+            if TrueCount != len(SpecificNameArr)-1:
                 TrueCount = TrueCount + 1
         else:
             NumpyArr.append(0)
 
     return NumpyArr
 
-print(GetNumpyArray())
+#Calling array to test
+print(GetNumpyArray(Jordan1MochaArr))
