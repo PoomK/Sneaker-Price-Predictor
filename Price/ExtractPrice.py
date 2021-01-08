1620ef350cef7ab18d5a37062535778612db3cbb
Poom Kijoran <triplepoom@hotmail.com>
2021-01-08 19:29:59 +0700

    Created function for extract date

--- Price/ExtractPrice.py
+++ Price/ExtractPrice.py
@@ -8,8 +8,12 @@ def get_price():
                 LineList = line.split("$")
                 price = LineList[1].strip()
 
-def extract_date():
+def extract_date(dateString):
     #Remove "Sunday,"
     dateString = ",".join(dateString.split(",")[1:]).strip()
-    datetimeObj = datetime.strptime(dateString, "%B %d, %Y %I:%M %P")
+    datetimeObj = datetime.strptime(dateString, "%B %d, %Y %I:%M %p")
     return datetimeObj
+
+#Extract date tester
+'''dateString1 = "Sunday, November 12, 2017 10:03 AM"
+print(extract_date(dateString1))'''
